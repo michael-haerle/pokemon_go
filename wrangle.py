a7912d2a6ab33438b6676b3e16cf676a2ab20cee
Michael Haerle <michael.haerle01@gmail.com>
2023-01-30 16:57:56 -0600

    added wrangle function

---
+++ wrangle.py
@@ -0,0 +1,18 @@
+# Standard imports
+import pandas as pd
+# Vis imports
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Stats imports
+from scipy.stats import spearmanr
+
+def wrangle_df():
+    df = pd.read_csv('comprehensive_dps_updated.csv')
+    df.rename(columns = {'DPS^3*TDO' : 'Battle_Stat'}, inplace = True)
+    df1 = pd.read_csv('Pokemon.csv')
+    df1.rename(columns = {'Name' : 'Pokemon'}, inplace = True)
+    df_best_attack_set = pd.read_csv('Best_Attack_Set_Updated.csv')
+    df_best_attack_set.drop("Unnamed: 0", axis='columns', inplace=True)
+    df_best_attack_set.drop("Unnamed: 20", axis='columns', inplace=True)
+    return df, df1, df_best_attack_set
+
